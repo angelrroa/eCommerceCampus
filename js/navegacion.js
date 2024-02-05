2cabd064caa41e5ec9419987bd59bbb38a0679d2
angelrroa <67547536+angelrroa@users.noreply.github.com>
2024-02-05 00:56:06 -0500

    feat: :beers: Ajuste de responsive y menu desplegable
    Se diseña el responsive para cada una de las tarjetas y se crea el menú desplegable para ambas medidas
    
    @CampusLands

--- js/navegacion.js
+++ js/navegacion.js
@@ -1,15 +1,25 @@
+// Función para cambiar la marca seleccionada
 function changeBrand(brand) {
-    document.getElementById('brandName').innerText = brand;
-  }
-  
-  const menuIcon = document.getElementById('menuIcon');
-  const menu = document.getElementById('menu');
-  
-  menuIcon.addEventListener('click', function() {
-    if (menu.style.display === 'block') {
-      menu.style.display = 'none';
+    // Lógica para cambiar la marca
+    document.getElementById("brandName").innerText = brand;
+    // Ocultar el menú después de hacer clic en un elemento
+    toggleMenu();
+}
+
+// Función para mostrar u ocultar el menú desplegable
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    var screenWidth = window.innerWidth;
+
+    if (screenWidth <= 768) {
+        // Si la pantalla es pequeña, se puede mostrar u ocultar el menú
+        if (menu.style.display === "flex") {
+            menu.style.display = "none";
+        } else {
+            menu.style.display = "flex";
+        }
     } else {
-      menu.style.display = 'block';
+        // Si la pantalla es grande, el menú debe permanecer visible
+        menu.style.display = "flex";
     }
-  });
-  
+}
