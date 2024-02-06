2bf8871d1e18c0acf1035444a4a040266792ad27
angelrroa <67547536+angelrroa@users.noreply.github.com>
2024-02-05 22:52:05 -0500

    feat: :sparkles: Creación del carrito de compras
    Se crea una nueva sección en el HTML con la finalidad de que se muestre cuando sea presionado el icono del carrito de compras, por ende se hizo necesario crear funcionalidad en js y a su vez darle estilo para que se acoplara al estilo de la pagina
    
    @CampusLands

--- js/navegacion.js
+++ js/navegacion.js
@@ -24,3 +24,18 @@ function toggleMenu() {
     }
 }
 
+function openModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
